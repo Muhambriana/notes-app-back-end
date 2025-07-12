07aba70f4db4bf20f3563e157bed0bcd56b03744
muham <muhambriana.31@gmail.com>
2025-07-13 00:38:57 +0700

    feat: add NotesService for managing notes functionality

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 const Hapi = require('@hapi/hapi');
-const notesPlugins =  require('./notes_plugins');
+const routes = require('./routes');
 
 const init = async () => {
-  const server = Hapi.server();
-
-  await server.register({
-    plugin: notesPlugins,
-    options: { notes:  [] }
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
+
+  server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
