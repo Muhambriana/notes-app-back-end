ca4b5675f725c1495c9dc0de9bdf31fc7a74d8bd
Muham <muhambriana.31@gmail.com>
2025-07-25 10:48:50 +0700

    fix: correct validation schema used in validatePutAuthenticationPayload method

--- src/validator/authentications/index.js
+++ src/validator/authentications/index.js
@@ -15,7 +15,7 @@ const AuthenticationsValidator = {
   },
 
   validatePutAuthenticationPayload: (payload) => {
-    const validationResult = DeleteAuthenticationPayloadSchema.validate(payload);
+    const validationResult = PutAuthenticationPayloadSchema.validate(payload);
 
     if (validationResult.error) {
       throw new InvariantError(validationResult.error.message);
