488c0b164f7bb1af314ccd6d4042b57078f91b99
muham <muhambriana.31@gmail.com>
2025-07-26 18:36:29 +0700

    fix: update note access verification method and correct SQL query in CollaborationsService

--- src/services/postgres/CollaborationsService.js
+++ src/services/postgres/CollaborationsService.js
@@ -39,7 +39,7 @@ class CollaborationsService {
 
   async verifyCollaborator(notedId, userId) {
     const query = {
-      text: 'SELECT * FROM colaborations WHERE notes_id = $1 AND user_id = $2',
+      text: 'SELECT * FROM collaborations WHERE note_id = $1 AND user_id = $2',
       values: [notedId, userId],
     };
 
