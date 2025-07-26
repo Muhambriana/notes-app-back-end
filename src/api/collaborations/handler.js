7fe410463307e8433c98ed524b9ae59765578324
muham <muhambriana.31@gmail.com>
2025-07-26 17:35:45 +0700

    feat: implement collaborations feature with post and delete handlers

---
+++ src/api/collaborations/handler.js
@@ -0,0 +1,46 @@
+class Collaborationshandler {
+  constructor(collaborativeService, notesService, validator) {
+    this._collaborativeService = collaborativeService;
+    this._notesService = notesService;
+    this._validator = validator;
+  }
+
+  async postCollaborationHandler(request, h) {
+    this._validator.validateCollaborationPayload(request.payload);
+
+    const { id: credentialId } = request.auth.credentials;
+    const { noteId, userId } = request.payload;
+
+    await this._notesService.verifyNoteOwner(noteId, credentialId);
+
+    const collaborationId = await this._collaborativeService.addCollaboration(noteId, userId);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Kolaborasi berhasil ditambahkan',
+      data: {
+        collaborationId,
+      }
+    });
+
+    response.code(201);
+    return response;
+  }
+
+  async deleteCollaborationHandler(request, h) {
+    this._validator.validateCollaborationPayload(request.payload);
+
+    const { id: credentialId } = request.auth.credentials;
+    const { noteId, userId } = request.payload;
+
+    await this._notesService.verifyNoteOwner(noteId, credentialId);
+    await this._collaborativeService.deleteCollaboration(noteId, userId);
+
+    return {
+      status: 'success',
+      message: 'Kolaborasi berhasil dihapus',
+    };
+  }
+}
+
+module.exports = Collaborationshandler;
