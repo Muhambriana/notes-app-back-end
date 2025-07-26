6568f1535fd8599e7e2a3cab027b0ccf94aca0ba
muham <muhambriana.31@gmail.com>
2025-07-26 11:41:10 +0700

    feat: add owner column to notes and implement authorization checks

---
+++ migrations/1753500914686_add-column-owner-to-table-notes.js
@@ -0,0 +1,25 @@
+const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const up = (pgm) => {
+  pgm.addColumn('notes', {
+    owner: {
+      type: 'VARCHAR(50)',
+    }
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const down = (pgm) => {
+  pgm.dropColumn('notes', 'owner');
+};
+
+module.exports = { shorthands, up, down };
